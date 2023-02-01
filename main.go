6741e68053cb8553448edca54bf709c9bcdd31c0
fadilaahmads <fadilahmad59@gmail.com>
2023-02-01 21:56:41 +0700

    Add handler func to route

--- main.go
+++ main.go
@@ -9,8 +9,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
+	r.SetTrustedProxies([]string{"127.0.0.1"})
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, models.WebResponse{
@@ -22,7 +40,7 @@ func main() {
 		})
 	})
 	r.GET("/getall", controllers.GetAll)
-	r.GET("/getone/:id")
+	r.GET("/getone/:id", controllers.GetOne)
 	r.POST("/create")
 	r.PATCH("/update/:id")
 	r.DELETE("/delete/:id")
